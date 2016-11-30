82355e5c093e5b2543a9d34cee9a8721ac6e310a
Dylan Morris <dhmorris@princeton.edu>
2016-11-29 22:13:14 -0500

    Commented model.h

--- src/model.h
+++ src/model.h
@@ -5,6 +5,10 @@
 
 using namespace Eigen;
 
+
+/* class to hold user-specified models of stochastic systems
+ so that realizations from them can be simulated */
+
 class Model {
  public:
   Model(int n_vars, int n_events,
@@ -13,10 +17,15 @@ class Model {
                                      int event_choice));
   ~Model();
 
-  const int n_vars;
-  const int n_events;
+  const int n_vars;    // length of state array
+  const int n_events;  // number of possible events
 
+  /* pointer to function that calculates current rates 
+     at which events occur */
   Vector_xf (*rate_function) (double t, VectorXd state_array);
+
+  /* pointer to function that calculates change to state 
+     array for a given event */
   Vector_xf (*event_function) (double t, VectorXd state_array,
                                int event_choice));
 }
