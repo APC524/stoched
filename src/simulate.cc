eb078e3a0cf2e0eedb1652c0f3c0660414e490f5
Kevin Griffin <kevinpg@princeton.edu>
2016-11-29 22:48:56 -0500

    Add the Eigen Library

---
+++ src/simulate.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "../lib/eigen/Eigen/Core"
+#include "../lib/eigen/Eigen/LU"
+//#include model.h
+
+using namespace Eigen;
+
+int main(int argc, char* argv[]){
+  printf("Hello, World!\n");
+  
+  int u_len = 4;
+  VectorXd u(u_len);
+  for (int i = 0; i < u_len; i++){
+    u(i) = i*4.3;
+  }
+  for  (int i = 0; i < u_len; i++){
+    printf("%f\n",u(i));
+  }
+}
