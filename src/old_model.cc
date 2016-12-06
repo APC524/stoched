74b9a97a5c5a0851fbf13b058cc5b81af2e6f874
Dylan Morris <dhmorris@princeton.edu>
2016-12-06 01:33:36 -0500

    Checked out old model files for prototyping, staying separate from new implementation

---
+++ src/old_model.cc
@@ -0,0 +1,17 @@
+#include "old_model.h"
+
+Model::Model(int n_vars,
+             int n_events,
+             Vector_xf (*rate_function) (double t, VectorXd state_array,
+                                         VectorXd rate_params),
+             Vector_xf (*event_function) (double t, VectorXd state_array,
+                                          VectorXd event_params,
+                                          int event_choice)
+             ):
+  n_vars(n_vars),
+  n_events(n_events),
+  (*rate_function)((*rate_function)),
+  (*event_function)((*event_function))
+{};
+
+Model::~Model(){};
