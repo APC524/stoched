a327665c9eb3ebfed89c7f7b255c3dad771f1942
juliennelachance <julienne.lachance@gmail.com>
2017-01-05 12:57:29 -0500

    Added OpenMP implementation

--- src/stoched.cc
+++ src/stoched.cc
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <iomanip>
 #include <chrono>
-
+#include <omp.h>
 #include "event.h"
 #include "model.h"
 #include "paramset.h"
@@ -12,7 +12,8 @@
 #include "xoroshiro128plus.h"
 
 
-using namespace std;
+//using namespace std;
+using namespace std::chrono;
 
 //declare the parser method written by flex and bison
 int parseFile(Model& model);
@@ -49,7 +50,7 @@ int main(int argc, char *argv[]) {
   string param_path = argv[2];
 
   // allow user-specified output path, or default
-  string out_path = "stoched_output.txt";
+  string out_path = "stoched_output";
   
   // declare model
   Model *model_ptr;
@@ -57,7 +58,6 @@ int main(int argc, char *argv[]) {
   model_ptr = & model;
 
   // populate model from parser
-
   int res = parseFile(model);
   if (res != 0) {
     fprintf(stderr, "Error: Parser returned %d", res);
@@ -74,7 +74,7 @@ int main(int argc, char *argv[]) {
   double t_initial = 0;
   double t_final = 5000;
   double timestep_size = 0.5;
-  double n_realizations = 1;
+  int n_realizations = 1;
   double max_iter = 100000000;
   int seed = 502;
   Paramset paramset(method, n_vars, inits, t_initial,
@@ -82,28 +82,46 @@ int main(int argc, char *argv[]) {
                     max_iter, seed);
 
   xoroshiro128plus* rng_ptr = new xoroshiro128plus(seed);
-  // Open file
-  ofstream myfile;
-  myfile.open(out_path);
 
-  // Write the header line corresponding to model
-  myfile << left << setw(15) << "time";
-  int vars_count = model.getVarsCount(); 
-  for(int i = 0; i < n_vars; i++){
-    string test = model.getIthVar(i);
-    myfile << left << setw(15) <<  test;
-  }
-  myfile << "\n";
+  // Instantiate timer 
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-  
-  /* instantiate realization class
-     corresponding to the user-specified
-     method (TO DO! Factory Pattern!) */
-  FirstReaction realization(model_ptr, paramset,
+  // Set up OMP environment/variables 
+  omp_set_dynamic(0);     // Explicitly disable dynamic teams
+  int nthreads = omp_get_max_threads();
+  omp_set_num_threads(nthreads);
+
+  // Loop over instantiations of realizations for same model: 
+  int i=0;
+  #pragma omp parallel for private(i)
+  for(i = 0; i < n_realizations; i++){
+    // Open file
+    ofstream myfile;
+    string write_out_path = out_path + "_event_" + to_string(i+1) + ".txt";
+
+    myfile.open(write_out_path);
+    // Write the header line corresponding to model
+    myfile << left << setw(15) << "time";
+    int vars_count = model.getVarsCount(); 
+    for(int i = 0; i < n_vars; i++){
+      string test = model.getIthVar(i);
+      myfile << left << setw(15) <<  test;
+    }
+    myfile << "\n";
+
+    /* instantiate realization class
+       corresponding to the user-specified
+       method (TO DO! Factory Pattern!) */
+    FirstReaction realization(model_ptr, paramset,
                               rng_ptr, n_vars, n_events);
-
-  // simulate and clean up
-  realization.simulate(myfile);
+    // simulate and clean up
+    realization.simulate(myfile);
+    myfile.close();
+  }
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  auto duration_first = duration_cast<microseconds>( t2 - t1 ).count();
+  printf("Test ran with %d threads \n", nthreads);
+  printf("Test ran in %15.8f seconds \n", duration_first * 1.0e-6);
 
   return 0;
 }
